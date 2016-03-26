83bac4df6f818d938884fc693190c9e9974f0f96
Yu <jinggeqianyu1991@gmail.com>
2016-03-26 18:04:22 -0400

    NOW use route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, useRouterHistory, hashHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import Detail from './pages/Detail';
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+
 ReactDOM.render(
-    <Detail message="This is coming from props!"/>,
+    <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
+        <Route path="/" component={ Detail } />
+    </Router>,
     document.getElementById('app')
 );
