ad9c14517f35e80fbec143ee5db414dec6c18657
Yu <jinggeqianyu1991@gmail.com>
2016-04-03 22:01:18 -0400

    separate routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, useRouterHistory, hashHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
+import App from './pages/App';
 import Detail from './pages/Detail';
 import List from './pages/List';
 
@@ -10,8 +11,10 @@ const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 ReactDOM.render(
     <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
-        <Route path="/" component={ List } />
-        <Route path="/detail/:repo" component={ Detail } />
+        <Route path="/" component={ App }>
+            <IndexRoute component={ List } />
+            <Route path="/detail/:repo" component={ Detail } />
+        </Route>
     </Router>,
     document.getElementById('app')
 );
---
+++ src/pages/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import Detail from './pages/routes';
+
+const routes = (
+    <Route path="/" component={ App }>
+        {routes}
+    </Route>
+);
+
+export default routes;
