3f52ab72fdb750df174cdfa7931c6de72050d70d
Yu <jinggeqianyu1991@gmail.com>
2016-04-09 10:30:07 -0400

    add routes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, useRouterHistory, hashHistory } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
-import App from './pages/App';
-import Detail from './pages/Detail';
-import List from './pages/List';
+import routes from './routes';
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 ReactDOM.render(
     <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
-        <Route path="/" component={ App }>
-            <IndexRoute component={ List } />
-            <Route path="/detail/:repo" component={ Detail } />
-        </Route>
+        {routes}
     </Router>,
     document.getElementById('app')
 );
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Detail from './pages/routes';
+import App from './pages/App';
+import List from './pages/List';
+import Detail from './pages/Detail';
 
 const routes = (
     <Route path="/" component={ App }>
-        {routes}
+        <IndexRoute component={ List } />
+        <Route path="detail/:repo" component={ Detail } />
     </Route>
 );
 
